5597eb10c34523ec3271878eb8f9955080d1213d
Shuming Liu <smliu@mit.edu>
2025-02-22 21:26:07 -0500

    Modify

---
+++ tests/check-explicit-ion/README.md
@@ -0,0 +1,4 @@
+# check-explicit-ion
+
+The lammps output files are saved in `lamamps-output`. The lammps system is composed of 215 CG atoms. CG atoms 1-59 are CG CA atoms from 1apl chain C. CG atoms 60-183 are CG DNA atoms from 1apl chain A and chain B. CG atoms 184-215 are NA+ ions. 
+
--- tests/check-explicit-ion/rerun.py
+++ tests/check-explicit-ion/rerun.py
@@ -24,8 +24,10 @@ Rerun the protein-DNA system with explicit ion model.
 """
 
 # separate the original pdb file into protein and dna pdb files
+# build the system consistent with lammps system
+# CG protein is chain C of 1apl
 df_atoms = parse_pdb('1apl.pdb')
-protein_atoms = df_atoms[df_atoms['resname'].isin(_amino_acids)].copy()
+protein_atoms = df_atoms[df_atoms['chainID'] == 'C'].copy()
 protein_atoms['serial'] = np.arange(len(protein_atoms.index)) + 1
 write_pdb(protein_atoms, 'protein.pdb')
 dna_atoms = df_atoms[df_atoms['resname'].isin(_dna_nucleotides)].copy()
@@ -38,28 +40,36 @@ dna_parser = DNA3SPN2Parser.from_atomistic_pdb('dna.pdb', 'cg_dna.pdb')
 model = SMOG3SPN2ExplicitIonModel()
 model.append_mol(protein_parser)
 model.append_mol(dna_parser)
-n_NA_ions = 514
-n_CL_ions = 482
+n_NA_ions = 32
+#n_CL_ions = 0
 model.append_ions('NA', n_NA_ions)
-model.append_ions('CL', n_CL_ions)
+#model.append_ions('CL', n_CL_ions)
+output_atoms = model.atoms.copy()
+output_atoms.loc[:, 'charge'] = '' # remove charge to avoid format error
+write_pdb(output_atoms, 'start.pdb')
 
+
+"""
 # prepare the pdb file to get topology
 box = [20, 20, 20]
 insert_molecules('cg_dna.pdb', 'cg_protein_dna.pdb', 1, existing_pdb='cg_protein.pdb', box=box)
 df_atoms = parse_pdb('cg_protein_dna.pdb')
-df_atoms = insert_molecules_dataframe(df_sodium_ion, n_NA_ions, existing_atoms=df_atoms, box=box)
-df_atoms = insert_molecules_dataframe(df_chloride_ion, n_CL_ions, existing_atoms=df_atoms, box=box)
+df_atoms = insert_molecules_dataframe(df_sodium_ion, n_NA_ions, 
+                                      existing_atoms=df_atoms, box=box)
+#df_atoms = insert_molecules_dataframe(df_chloride_ion, n_CL_ions, existing_atoms=df_atoms, box=box)
 is_NA = df_atoms['name'] == 'NA'
 is_CL = df_atoms['name'] == 'CL'
 df_atoms.loc[is_NA, 'resSeq'] = 1 + np.arange(n_NA_ions)
-df_atoms.loc[is_CL, 'resSeq'] = 1 + np.arange(n_CL_ions)
+#df_atoms.loc[is_CL, 'resSeq'] = 1 + np.arange(n_CL_ions)
 df_atoms.loc[:, 'charge'] = '' # remove charge information here to avoid pdb format error
 #df_atoms.to_csv('cg_atoms.csv', index=False)
 write_pdb(df_atoms, 'start.pdb', write_TER=True) # start.pdb includes CG protein, DNA, and ions
 top = app.PDBFile('start.pdb').getTopology()
 #print(f'{top.getNumAtoms()} atoms')
+"""
 
 # create openmm system and compute energies
+top = app.PDBFile('start.pdb').getTopology()
 model.create_system(top, box_a=20, box_b=20, box_c=20)
 model.add_protein_bonds(force_group=1)
 model.add_protein_angles(force_group=2)
@@ -75,9 +85,11 @@ model.parse_all_exclusions() # set exclusions before adding nonbonded forces
 model.add_all_vdwl_elec(force_group_sr=11, force_group_PME=12)
 system = model.system
 #print(f'{system.getNumParticles()} atoms')
-#traj = mdtraj.load_dcd('lammps-run/DUMP_FILE_temp300.dcd', top='start.pdb')
-#energy = compute_PE(traj, system, 'CUDA', properties={'Precision', 'double'})
-#print(energy)
+dcd = 'lammps-output/DUMP_Extract_temp300.dcd'
+traj = mdtraj.load_dcd(dcd, top='start.pdb')
+print(f'{dcd} has {traj.n_frames} frames')
+energy = compute_PE(traj, system, 'CUDA', properties={'Precision': 'double'})
+print(energy)
 
 
 
